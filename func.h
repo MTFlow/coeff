819def7c16ab397bf3f38d39acdf656727ccdd24
MTFlow <m.c.w.wolf@tue.nl>
2019-05-27 12:42:00 +0100

    add

---
+++ func.h
@@ -0,0 +1,128 @@
+//===============flux=========================
+void flux(int S, double *Left_bnd)
+{
+	int iVX,iVY,iVZ;
+
+	int id = buf[iid+size_one*S];
+	double z = buf[iz+size_one*S];
+	
+	double z_left[4] = {Left_bnd[0] + 10.0, Left_bnd[1] - 10.0, 
+											Left_bnd[2] + 10.0, Left_bnd[3] - 10.0};
+
+//	int pos;
+	int sign_condL, sign_collL, sign_evapL, sign_outL;
+//	int sign_condR, sign_collR, sign_evapR, sign_outR;
+
+	sign_outL  = (z > Left_bnd[3]) - 2; // sign -1 or -2
+	sign_collL = (z > Left_bnd[2]) - 2; // sign
+//	sign_evapL = (z > Left_bnd[1]) - 2; // sign
+	sign_condL = (z > Left_bnd[0]) - 2; // sign
+
+
+	if (vflag) {
+		double VX = buf[ivx+size_one*S];
+		double VY = buf[ivy+size_one*S];
+		double VZ = buf[ivz+size_one*S];
+
+		double iivx = floor((VX-vmin)*iV/(vmax-vmin));
+		double iivy = floor((VY-vmin)*iV/(vmax-vmin));
+		double iivz = floor((VZ-vmin)*iV/(vmax-vmin));
+
+		iVX = (int)sqrt(iivx*iivx);
+		iVY = (int)sqrt(iivy*iivy);
+		iVZ = (int)sqrt(iivz*iivz);
+	};
+
+	//====Jout====//
+	if (JoutL_oldlist[id] != 0) {
+		if (JoutL_oldlist[id] == sign_outL) { //if old sign == new sign => atom did not cross fzhi
+//			if (z <= Left_bnd[3]) {
+				JoutL_newlist[id] = sign_outL;		
+//			}
+		} else {
+//		if (JoutL_oldlist[id] != sign_outL) {
+				JL[3]++;
+				if (vflag) {
+					vel_Jout[iVX] += 1.0;
+					vel_Jout[iVY+iV] += 1.0;
+					vel_Jout[iVZ+2*iV] += 1.0;
+				};
+//				JoutL_newlist[id] = 0;		
+				if (indices[id] == -2) { //flag[pos_flag+1] == -2) {
+					JL[1]++; // old sign /= new sign
+					if (vflag) {
+						vel_Jevap[iVX] += 1.0;
+						vel_Jevap[iVY+iV] += 1.0;
+						vel_Jevap[iVZ+2*iV] += 1.0;
+					};
+				};
+		}
+	} else if (z < Left_bnd[3]) {
+		JoutL_newlist[id] = sign_outL;		
+	}
+
+/*
+	//====Jevap====//
+	if (JevapL_oldlist[id] != 0) {
+		if (JevapL_oldlist[id] == sign_evapL) { //if old sign == new sign => atom did not cross fzhi
+//			if (z <= Left_bnd[1]) {
+					JevapL_newlist[id] = sign_evapL;
+//			}
+		} else {
+//			if (JevapL_oldlist[id] != sign_evapL) {
+			if (indices[id] == -2) { //flag[pos_flag+1] == -2) {
+				JL[1] += 1; // old sign /= new sign
+				vel_Jevap[iVX+VB_tic*3*iV] += 1.0;
+				vel_Jevap[iVY+iV+VB_tic*3*iV] += 1.0;
+				vel_Jevap[iVZ+2*iV+VB_tic*3*iV] += 1.0;
+				JevapL_newlist[id] = 0;
+			};
+		}
+	} else if (z <= Left_bnd[1]) {
+			JevapL_newlist[id] = sign_evapL;
+	};
+*/
+
+	//====Jcoll====//
+	if (JcollL_oldlist[id] != 0) {
+		if (JcollL_oldlist[id] == sign_collL) { //if old sign == new sign => atom did not cross fzhi
+//			if (z >= Left_bnd[2] && z <= z_left[2]) {
+				JcollL_newlist[id] = sign_collL;		
+//			}
+	} else {
+			JL[2]++;
+			if (vflag) {
+				vel_Jcoll[iVX] += 1.0;
+				vel_Jcoll[iVY+iV] += 1.0;
+				vel_Jcoll[iVZ+2*iV] += 1.0;
+			};
+//			JcollL_newlist[id] = 0;		
+		}
+	} else if (z >= Left_bnd[2]) {
+			JcollL_newlist[id] = sign_collL;		
+	}
+
+
+
+	//====Jcond====//
+	if (JcondL_oldlist[id] != 0) {
+			if (JcondL_oldlist[id] == sign_condL) { //if old sign == new sign => atom did not cross fzhi
+//				if (z >= Left_bnd[0] && z <= z_left[0]) {
+					JcondL_newlist[id] = sign_condL;
+//			}
+		} else {
+			if (indices[id] == -1) { //flag[pos_flag+1] == -1) {
+				JL[0]++; // old sign /= new sign
+				if (vflag) {
+					vel_Jcond[iVX] += 1.0;
+					vel_Jcond[iVY+iV] += 1.0;
+					vel_Jcond[iVZ+2*iV] += 1.0;
+				};
+//				JcondL_newlist[id] = 0;
+			};
+		}
+	} else if (z >= Left_bnd[0]) {
+			JcondL_newlist[id] = sign_condL;
+	}
+
+}
---
+++ main.h
@@ -0,0 +1,85 @@
+#include "stdint.h"
+#define __STDC_FORMAT_MACROS
+#include "inttypes.h"
+
+#ifndef PRId64
+#define PRId64 "ld"
+#endif
+#if !defined(LAMMPS_SMALLSMALL) && !defined(LAMMPS_BIGBIG) && !defined(LAMMPS_SMALLBIG)
+#define LAMMPS_SMALLBIG
+#endif
+
+#if defined(LAMMPS_SMALLBIG)
+typedef int tagint;
+typedef int64_t bigint;
+#define BIGINT_FORMAT "%" PRId64
+#elif defined(LAMMPS_SMALLSMALL)
+typedef int tagint;
+typedef int bigint;
+#define BIGINT_FORMAT "%d"
+#else /* LAMMPS_BIGBIG */
+typedef int64_t tagint;
+typedef int64_t bigint;
+#define BIGINT_FORMAT "%" PRId64
+#endif
+
+
+inline bool fexists (const std::string& name) {
+  struct stat buffer;   
+  return (stat (name.c_str(), &buffer) == 0); 
+}
+
+int n;
+bigint ntimestep,natoms;
+int size_one,nchunk,triclinic;
+double xlo,xhi,ylo,yhi,zlo,zhi,xy,xz,yz;
+int boundary[3][2];
+//char boundstr[9];
+
+char *lineD = NULL;
+char *lineS = NULL;
+
+int VB_tic;
+
+int *vel_Jout = NULL;
+int *vel_Jcoll = NULL;
+int *vel_Jevap = NULL;
+int *vel_Jcond = NULL;
+
+double *indexID = NULL;
+int *indices = NULL;
+int *JevapL_oldlist = NULL;
+int *JevapL_newlist = NULL;
+int *JcondL_oldlist = NULL;
+int *JcondL_newlist = NULL;
+int *JcollL_oldlist = NULL;
+int *JcollL_newlist = NULL;
+int *JoutL_oldlist = NULL;
+int *JoutL_newlist = NULL;
+
+int data,vflag;
+//int *Jold = NULL;
+//int *Jnew = NULL;
+
+int *JL = NULL;
+
+FILE *screen = stdout;
+FILE *error = stderr;
+FILE *fpdatvel = NULL;
+
+int imass,imol,iid,itype,ix,iy,iz,ivx,ivy,ivz,ike,ipe,is1,is2,is3,is4,is5,is6;
+int STRflag,output_every_flag;
+int start,stop,N,output,step_size,threads,iV,output_every;
+double LB_start,LB_stop,iLB,VB_start,VB_stop,iVB,vmin,vmax;
+//double *coord = NULL;
+
+char *STR = NULL;
+
+int maxbuf = 0;
+double *buf = NULL;
+
+const double Na = 6.02214086e23; // 1/mol
+const double kb = 1.38064852e-23; // J/K
+const double R = Na * kb;
+const double Pi = 3.141592653589793;
+
